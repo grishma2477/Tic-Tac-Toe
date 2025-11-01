5d00482411585664f6a3b6a572cc3fcd8b6cded6
Grishma2477 <kandelgrishma2477@gmail.com>
2025-11-02 10:08:48 +1100

    :construction: Lifting the state up

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,13 +7,13 @@ const initialGameBoard = [
 ];
 
 
-export const GameBoard = ({onSelectSquare}) => {
+export const GameBoard = ({onSelectSquare,activePlayerSymbol}) => {
     const [gameBoard, setGameBoard] = useState(initialGameBoard);
     
     function handleSelectSquare(rowIndex, colIndex){
         setGameBoard((prevGameBoard) =>{
             const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
-            updatedBoard[rowIndex][colIndex] = 'X';
+            updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
             return updatedBoard;
         });
 
