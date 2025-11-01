554ace11d78d4e1039aafd9190a0f11ce1dc0d69
Grishma2477 <kandelgrishma2477@gmail.com>
2025-11-01 11:12:45 +1100

    :construction: Working on Tic-Tac-Toe game

---
+++ src/components/Player.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+export const Player = ({initialName, symbol})=>{
+    const [playerName, setPlayerName] = useState(initialName);
+    const [isEditing, setIsEditing] = useState(false);
+
+
+    const handleEditClick = () => {
+        setIsEditing((editing) => !editing);
+    }    
+    
+    const handleChange = (e) => {
+        setPlayerName(e.target.value);
+    }
+
+   let editablePlayerName =  <span className="player-name">{playerName}</span>
+
+    if (isEditing) {
+        editablePlayerName = <input type="text" required value={playerName} onChange={handleChange}/>;
+    }
+
+
+    return (
+    <li>
+    <span className="player">
+   {editablePlayerName}
+    <span className="player-symbol">{symbol}</span>
+    </span>
+    <button onClick={handleEditClick}>{isEditing ? 'Save' : 'Edit' }</button>
+     </li>
+    )
+}
