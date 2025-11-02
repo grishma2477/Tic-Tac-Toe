d410cc3e0dda0e276b37f29c131c08331a592693
Grishma2477 <kandelgrishma2477@gmail.com>
2025-11-03 09:59:40 +1100

    :construction: rematch button enabled and improved the player's info

--- src/App.jsx
+++ src/App.jsx
@@ -24,13 +24,18 @@ const deriveActivePlayer = (gameTurns)=>{
 }
 
 export const App = ()=> {
+  const [players, setPlayers] = useState({
+    X:'Player 1',
+    O: 'Player 2'
+  });
+
   const [gameTurns, setGameTurns] = useState([]);
   // const [hasWinner, setHasWinner] = useState(false);
   // const [activePlayer, setActivePlayer] = useState('X');
 
     const activePlayer = deriveActivePlayer(gameTurns);
 
-   let gameBoard = initialGameBoard;
+   let gameBoard = [...initialGameBoard.map(array =>[...array])];
 
     for (const turn of gameTurns) {
         const {square, player} = turn;
@@ -67,7 +72,19 @@ export const App = ()=> {
     });
   } 
 
-  
+  const handleRestart = () => {
+    setGameTurns([]);
+  }
+
+  const handlePlayerNameChange = (symbol, newName)=>{
+  setPlayers(prevPlayers =>{
+    return {
+      ...prevPlayers,
+      [symbol]: newName
+    };
+  });
+  }
+
   return <main>
     <div id="game-container">
     <ol id="players" className="highlight-player">
@@ -83,7 +100,7 @@ export const App = ()=> {
     isActive={activePlayer === 'O'}
     />
     </ol>
-    {(winner || hasDraw ) && <GameOver winner={winner}/>}
+    {(winner || hasDraw ) && <GameOver winner={winner} onRestart={handleRestart}/>}
     <GameBoard 
     onSelectSquare={handleSelectSquare} 
     board={gameBoard}
--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,10 +1,10 @@
-export const GameOver = ({winner})=>{
+export const GameOver = ({winner, onRestart})=>{
     return <div id="game-over">
         <h2>Game Over!</h2>
         {winner && <p>Player '{winner}'' won!</p>}
         {!winner && <p>It's a draw!</p>}
         <p>
-            <button>Rematch!</button>
+            <button onClick={onRestart}>Rematch!</button>
         </p>
     </div>
 };
